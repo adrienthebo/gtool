a77a70b4f7874c8122552d7ab422dc556ea7980c
Adrien Thebo <adrien@puppetlabs.com>
2011-11-14 19:29:40 -0800

    Added members and addmember methods for group.

--- lib/gtool/provision/group.rb
+++ lib/gtool/provision/group.rb
@@ -23,6 +23,32 @@ class Gtool
 
         print_table rows
       end
+
+      desc "members GROUP", "Display members of a group"
+      def members(group)
+        settings = Gtool::Auth.load_auth
+        connection = GData::Connection.new(settings[:domain], settings[:token])
+
+        group = GData::Provision::Group.get(connection, group)
+
+        members = group.list_members
+
+        fields = [:member_id, :member_type, :direct_member]
+        rows = members.map do |member|
+          fields.map {|f| member.send f}
+        end
+
+        print_table rows
+      end
+
+      desc "addmember GROUP MEMBER", "Add a member to a group"
+      def addmember(group, member)
+        settings = Gtool::Auth.load_auth
+        connection = GData::Connection.new(settings[:domain], settings[:token])
+        group = GData::Provision::Group.get(connection, group)
+
+        group.add_member member
+      end
     end
   end
 end
