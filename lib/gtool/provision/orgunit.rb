b4ced387aa1c7ad1d14f63de67991b7e4b77a8f3
Adrien Thebo <adrien@puppetlabs.com>
2011-12-21 14:35:55 -0800

    Added orgunit subcommand + list task

---
+++ lib/gtool/provision/orgunit.rb
@@ -0,0 +1,35 @@
+require 'thor'
+require 'gtool'
+require 'gdata'
+
+module Gtool
+  module Provision
+    class OrgUnit < Thor
+      Gtool::CLI.register self, "orgunit", "orgunit [COMMAND]", "GData user provisioning"
+      namespace :orgunit
+
+      class_option "debug", :type => :boolean, :desc => "Enable debug output", :aliases => "-d"
+      class_option "noop",  :type => :boolean, :desc => "Enable noop mode", :aliases => "-n"
+
+      desc "list", "List organizational units"
+      def list
+        connection = Gtool::Auth.connection(options)
+        orgunits = GData::Provision::OrgUnit.all(connection)
+        fields = GData::Provision::OrgUnit.attribute_names
+        field_names = GData::Provision::OrgUnit.attribute_titles
+
+        rows = orgunits.map do |orgunit|
+          fields.map {|f| orgunit.send f}
+        end
+
+        rows.unshift field_names
+        print_table rows
+        say "#{rows.length - 1} entries.", :cyan
+      end
+
+      def self.banner(task, namespace = true, subcommand = false)
+        "#{basename} #{task.formatted_usage(self, true, subcommand)}"
+      end
+    end
+  end
+end
