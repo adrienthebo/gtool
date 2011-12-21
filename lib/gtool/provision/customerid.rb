2ecd4e9e2220a4e8b4163f0befc45aa9c0abd9c0
Adrien Thebo <adrien@puppetlabs.com>
2011-12-21 12:35:21 -0800

    Added customerid class

---
+++ lib/gtool/provision/customerid.rb
@@ -0,0 +1,25 @@
+require 'thor'
+require 'thor/group'
+require 'gtool'
+require 'gdata'
+
+module Gtool
+  module Provision
+    class CustomerID < Thor::Group
+      Gtool::CLI.register self, "customerid", "customerid", "Display Customer ID for the domain"
+
+      class_option "debug", :type => :boolean, :default => false, :desc => "Enable debug output", :aliases => "-d"
+      class_option "noop",  :type => :boolean, :default => false, :desc => "Enable noop mode", :aliases => "-n"
+
+      def display
+        connection = Gtool::Auth.connection(options)
+        fields = GData::Provision::CustomerID.attribute_names
+        field_names = GData::Provision::CustomerID.attribute_titles
+        id = GData::Provision::CustomerID.get(connection)
+
+        properties = fields.map {|f| id.send f}
+        print_table field_names.zip(properties)
+      end
+    end
+  end
+end
