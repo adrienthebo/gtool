36e6b2bb67262d4cf1d9d3876899c31fdfd01120
Adrien Thebo <adrien@puppetlabs.com>
2012-01-25 21:40:39 -0800

    Collapse orgmember allmembers and members into list method

--- lib/gtool/provision/orgmember.rb
+++ lib/gtool/provision/orgmember.rb
@@ -11,31 +11,21 @@ module Gtool
       class_option "debug", :type => :boolean, :desc => "Enable debug output", :aliases => "-d"
       class_option "noop",  :type => :boolean, :desc => "Enable noop mode", :aliases => "-n"
 
-      desc "members ORGUNIT", "Get the members of an orgunit"
-      def members(orgunit_name)
+      desc "list", "Get the members of an orgunit"
+      method_option "orgunit", :type => :string, :desc => "The orgunit from which to retrive users"
+      def list
+        if options[:orgunit]
+          options_hash = {:target => :orgunit, :orgunit => options[:orgunit]}
+        else
+          options_hash = {:target => :all}
+        end
+
         connection = Gtool::Auth.connection(options)
-        orgmembers = GData::Provision::OrgMember.all(connection, :target => :orgunit, :orgunit => orgunit_name)
+        orgmembers = GData::Provision::OrgMember.all(connection, options_hash)
         fields = GData::Provision::OrgMember.attribute_names
         field_names = GData::Provision::OrgMember.attribute_titles
 
         rows = orgmembers.map do |member|
-          fields.map {|f| member.send f}
-        end
-
-        rows.unshift field_names
-        print_table rows
-        say "#{rows.length - 1} entries.", :cyan
-      end
-
-      desc "allmembers", "Retrieve all organization users"
-      def allmembers
-        connection = Gtool::Auth.connection(options)
-        fields = GData::Provision::OrgMember.attribute_names
-        field_names = GData::Provision::OrgMember.attribute_titles
-
-        members = GData::Provision::OrgMember.all(connection, :target => :all)
-
-        rows = members.map do |member|
           fields.map {|f| member.send f}
         end
 
